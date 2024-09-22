02aae0e5436180bb269c4538d06937411fb4cf4b
Matt Hamilton <matt.hamilton@berkeley.edu>
2024-09-22 19:51:51 +0000

    heroku url added to to NEW page form

--- app.rb
+++ app.rb
@@ -49,7 +49,7 @@ class WordGuesserApp < Sinatra::Base
     erb :show
   end
 
-  post '/create' do
+  get '/create' do
     # NOTE: don't change next line - it's needed by autograder!
     word = params[:word] || WordGuesserGame.get_random_word
     # NOTE: don't change previous line - it's needed by autograder!
